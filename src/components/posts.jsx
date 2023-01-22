70d3a006acf6b12946bbc6d8ac4ca2f3bcb7d406
Jamaloooo <jamalsa3d2001@gmail.com>
2023-01-22 21:10:38 +0200

    10- Query String Parameters

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import queryString from "query-string";
 
-const Posts = ({ match }) => {
+const Posts = ({ match, location }) => {
+  const result = queryString.parse(location.search);
+  console.log(result);
+
   return (
     <div>
       <h1>Posts</h1>
