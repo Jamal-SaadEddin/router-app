dcccb773640f51047134b4739eaed938c212d897
Jamaloooo <jamalsa3d2001@gmail.com>
2023-01-22 20:17:42 +0200

    8- Route Parameters

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Products extends Component {
   state = {
     products: [
       { id: 1, name: "Product 1" },
       { id: 2, name: "Product 2" },
-      { id: 3, name: "Product 3" }
-    ]
+      { id: 3, name: "Product 3" },
+    ],
   };
 
   render() {
@@ -14,9 +15,9 @@ class Products extends Component {
       <div>
         <h1>Products</h1>
         <ul>
-          {this.state.products.map(product => (
+          {this.state.products.map((product) => (
             <li key={product.id}>
-              <a href={`/products/${product.id}`}>{product.name}</a>
+              <Link to={`/products/${product.id}`}>{product.name}</Link>
             </li>
           ))}
         </ul>
