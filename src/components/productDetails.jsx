47cba97d3f242325739a9fb73cf563a7b3b3d901
Jamaloooo <jamalsa3d2001@gmail.com>
2023-01-23 08:57:23 +0200

    12- Programmatic Navigation

--- src/components/productDetails.jsx
+++ src/components/productDetails.jsx
@@ -2,7 +2,13 @@ import React, { Component } from "react";
 
 class ProductDetails extends Component {
   handleSave = () => {
-    // Navigate to /products
+    // Navigate to /products --> you can't go back
+    this.props.history.replace("/products");
+  };
+
+  handleNext = () => {
+    // Navigate to /products --> you can go back
+    this.props.history.push("/products");
   };
 
   render() {
@@ -10,6 +16,7 @@ class ProductDetails extends Component {
       <div>
         <h1>Product Details - {this.props.match.params.id} </h1>
         <button onClick={this.handleSave}>Save</button>
+        <button onClick={this.handleNext}>Next</button>
       </div>
     );
   }
